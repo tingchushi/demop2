091a1ee3613f728e9872ddb4a621bd255b8d7e6d
chushi <tingchushi@hotmail.com>
2022-09-03 09:55:52 +0800

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-function App() {
+const url =
+  "https://api.themoviedb.org/3/trending/tv/day?api_key=c33b955b56b34e6ac8cf34064ef78bbf";
+
+fetch(url)
+.then((response) => response.json())
+.then((data) => console.log(data));
+
+  function App() {
 
   return (
-    <div>
+    <div className="App">
       <h1>Demo</h1>
     </div>
   )
 }
 
 export default App
+
+// const url =
+//   "https://api.themoviedb.org/3/trending/movie/day?api_key=c33b955b56b34e6ac8cf34064ef78bbf";
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Demo</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
